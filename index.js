b576a63b87ac02ea4670929ba22757bf6af2daa4
Ankur <ankursinghagra@gmail.com>
2018-10-28 11:19:25 +0530

    added router

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 var port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'+ port))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
+var api_auth = require('./api/auth')
+
+app.use('/auth', api_auth)
